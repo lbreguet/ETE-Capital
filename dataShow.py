8ad81385e8d418cf73a46761ae14002ad114535c
lbreguet <louis-w@live.com>
2020-07-19 19:19:12 -0400

    Json to excel -table is populated with json data -nested data needs work -contracts aren't lsiting correctly

--- dataShow.py
+++ dataShow.py
@@ -1,90 +1,36 @@
-import getopt
 import json
+import openpyxl
+import pandas as pd
+from pandas import json_normalize
 import requests
-import sys
 
 
-def getMarkets():
+
+
+def get_markets():
     
-    data = bytesToDict(readXML('https://www.predictit.org/api/marketdata/all'))
+    data = bytes_to_dict(read_xml('https://www.predictit.org/api/marketdata/all'))
+   
     data = json.loads(data)
+    data = data['markets']
+    
+    df = pd.DataFrame(data)
+    df.to_excel('markets.xlsx')
+    return df
 
-    return data['markets']
+def read_xml(url):
 
-def readXML(URL):
+    r = requests.get(url)
 
-    r = requests.get(URL)
     content = r.content
-
+    
+ 
     return content
 
 
-def bytesToDict(data):
+def bytes_to_dict(data):
+
     return data.decode()
 
-def printData(args):
 
-    search = getopt.getopt(args, ':')
-    searchString = ""
-    searchString = searchString.join(search[1])
-
-    markets = getMarkets()
-
-
-    for market in markets:
-        
-        contracts = market['contracts']
-
-        if (len(contracts) >= 2):
-            
-            if searchString in market['name']:
-
-                name = market['name']
-                status = market['status']
-
-                print(name)
-                print(status)
-                print('Contracts:')
-                for contract in contracts:
-                    cname = contract['name']
-                    cstatus = contract['status']
-                    last_trade_price = contract['lastTradePrice']
-                    best_buy_yes_cost = contract['bestBuyYesCost']
-                    best_buy_no_cost = contract['bestBuyNoCost']
-                    best_sell_yes_cost = contract['bestSellYesCost']
-                    best_sell_no_cost = contract['bestSellNoCost']
-                    last_close_price = contract['lastClosePrice']
-
-                    if (last_trade_price is None):
-                        last_trade_price = 0.0
-
-                    if (best_buy_yes_cost is None):
-                        best_buy_yes_cost = 0.0
-
-                    if (best_buy_no_cost is None):
-                        best_buy_no_cost = 0.0
-
-                    if (best_sell_yes_cost is None):
-                        best_sell_yes_cost = 0.0
-
-                    if (best_sell_no_cost is None):
-                        best_sell_no_cost = 0.0
-
-                    if (last_close_price is None):
-                        last_close_price = 0.0
-
-                    print(cname)
-                    print(cstatus)
-                    print("Last Trade Price: " + str(last_trade_price))
-                    print("Best Buy Yes Cost: " + str(best_buy_yes_cost))
-                    print("Best Buy No Cost: " + str(best_buy_no_cost))
-                    print("Best Sell Yes Cost: " + str(best_sell_yes_cost))
-                    print("Best Sell No Cost: " + str(best_sell_no_cost))
-                    print("Last Close Price: " + str(last_close_price))
-                
-                    print()
-            
-            
-                print('----------------------------------')
-
-printData(sys.argv[1:])
+get_markets()
---
+++ run.ps1
@@ -0,0 +1,2 @@
+py ./dataShow.py
+py ./json_to_csv.py markets test.json test.csv
